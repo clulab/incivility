d1cb3e3760b2c2fd02cbf7cab171c99cde43ba60
Steven Bethard <steven.bethard@gmail.com>
2020-06-28 08:52:01 -0700

    Removes pre-classification-head dropout since it doesn't seem to matter.

--- models.py
+++ models.py
@@ -10,14 +10,11 @@ def from_transformer(transformer: transformers.TFPreTrainedModel,
     mask_inputs = tf.keras.Input(shape=(None,), name='mask_inputs', dtype='int32')
     segment_inputs = tf.keras.Input(shape=(None,), name='segment_inputs', dtype='int32')
 
-    # Load model and collect encodings
+    # get contextualized token encodings from transformer
     token_encodings = transformer([token_inputs, mask_inputs, segment_inputs])[0]
 
-    # Average token encodings
+    # get a sentence encoding from the token encodings
     sentence_encoding = tf.keras.layers.GlobalMaxPooling1D()(token_encodings)
-
-    # Apply dropout
-    sentence_encoding = tf.keras.layers.Dropout(0.1)(sentence_encoding)
 
     # Final output layer
     outputs = tf.keras.layers.Dense(n_outputs, activation='sigmoid', name='outputs')(sentence_encoding)
